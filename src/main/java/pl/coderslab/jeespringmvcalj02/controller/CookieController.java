dc09601c8f7718653876b0579d4d7ae9a7686750
edu <edu@mail.pl>
2023-02-12 15:02:02 +0100

    m5d2t3z1 cookies

---
+++ src/main/java/pl/coderslab/jeespringmvcalj02/controller/CookieController.java
@@ -0,0 +1,74 @@
+package pl.coderslab.jeespringmvcalj02.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+// Zadanie 1 - rozwiązywane z wykładowcą
+//W projekcie stwórz kontroller CookieController oraz trzy akcje dostępne pod adresami setCookie, showCookie, deleteCookie.
+//Akcja pod adresem /setCookie ma nastawiać ciasteczko o nazwie User na CodersLab, z ważnością 24h.
+//Akcja pod adresem /showCookie. Ma wyświetlać zawartość ciasteczka User.
+//Akcja pod adresem /deleteCookie ma kasować ciasteczko o nazwie User. Jeżeli nie ma takiego ciasteczka, to powinien wyświetlić się napis: BRAK.
+
+@Controller
+@RequestMapping("/cookie")
+public class CookieController {
+    @GetMapping("/setCookie")
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        Cookie c = new Cookie("User", "CodersLab");
+        c.setMaxAge(24 * 60 * 60);
+        c.setPath("/");
+
+        response.addCookie(c);
+
+        return "ciasteczko ustawione, zobacz w dev tools!";
+    }
+
+    @GetMapping("/showCookie")
+    @ResponseBody
+    public String showCookie(HttpServletRequest request) {
+        Cookie user = WebUtils.getCookie(request, "User");
+        if(user != null) {
+            return "ciasteczko odczytane, User=" + user;
+        }
+//        Cookie[] cookies = request.getCookies();
+//
+//        for(Cookie c : cookies) {
+//            if(c.getName().equals("User")) {
+//                return "ciasteczko odczytane, User=" + c.getValue();
+//            }
+//        }
+
+        return "brak ciasteczka";
+    }
+
+    @GetMapping("/showCookie2")
+    @ResponseBody
+    public String showCookie(@CookieValue(value = "User", required = false) String user) {
+        if(user != null) {
+            return "ciasteczko odczytane, User=" + user;
+        }
+        return "brak ciasteczka";
+    }
+
+    @GetMapping("/deleteCookie")
+    @ResponseBody
+    public String deleteCookie(HttpServletResponse response) {
+        Cookie c = new Cookie("User", "");
+        c.setMaxAge(0);
+        c.setPath("/");
+
+        response.addCookie(c);
+
+        return "ciasteczko usunięte, zobacz w dev tools!";
+    }
+}
